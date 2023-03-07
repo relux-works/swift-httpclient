482d3cb7c3884a6d7fdd98f6824053bc8ec46cfb
Alexey Grigorev <alexis.ag85@gmail.com>
2023-03-07 20:38:25 +0300

    updating wsPublished client

--- Sources/WSClient/OnPublisher/PublishedWSClient.swift
+++ Sources/WSClient/OnPublisher/PublishedWSClient.swift
@@ -2,32 +2,28 @@ import Foundation
 import Combine
 
 public protocol IPublishedWSClient {
-    func connect(to urlPath : String, with headers: @escaping ()->Headers) async -> Result<Void, WSClientError>
-    func disconnect() async
+    func connect(to urlPath : String, with headers: @escaping ()->Headers) -> Result<Void, WSClientError>
+    func connect(to urlPath : String) -> Result<Void, WSClientError>
+    func disconnect()
     func send(_ message: String) async -> Result<Void, WSClientError>
     func send(_ data: Data) async -> Result<Void, WSClientError>
-    var msgPublisher: AnyPublisher<Result<Data?, WSClientError>, Never> { get async }
+    var msgPublisher: AnyPublisher<Result<Data?, WSClientError>, Never> { get }
 }
 
-public actor PublishedWSClient: IPublishedWSClient, IRequestBuilder {
+public class PublishedWSClient: NSObject, IPublishedWSClient, IRequestBuilder {
     enum KeepConnected {
         case on(url: String, headers: ()->Headers)
         case off
     }
     private var webSocketTask: URLSessionWebSocketTask?
-    private let sessionConfig: URLSessionConfiguration
     @Published private var keepConnected: KeepConnected = .off
     private var keepAliveSubscription: AnyCancellable?
-    private let pingDelay: UInt32 = 5
+    private let pingDelay: UInt32
 
-    public init(
-        sessionConfig: URLSessionConfiguration = ApiSessionConfigBuilder.buildConfig(
-            timeoutForResponse: 20,
-            timeoutResourceInterval: 20
-        )
-    ) {
-        self.sessionConfig = sessionConfig
-        Task { await self.keepAlivePipeline() }
+    public init(pingInterval: UInt32 = 10) {
+        self.pingDelay = pingInterval
+        super.init()
+        self.keepAlivePipeline()
     }
 
     deinit {
@@ -37,13 +33,17 @@ public actor PublishedWSClient: IPublishedWSClient, IRequestBuilder {
     private var msgSubj = PassthroughSubject<Result<Data?, WSClientError>, Never>()
     public var msgPublisher: AnyPublisher<Result<Data?, WSClientError>, Never> { msgSubj.eraseToAnyPublisher() }
 
-    public func connect(to urlPath : String, with headers: @escaping ()->Headers) async -> Result<Void, WSClientError> {
+    public func connect(to urlPath : String) -> Result<Void, WSClientError> {
+        connect(to: urlPath, with: {[:]})
+    }
+
+    public func connect(to urlPath : String, with headers: @escaping ()->Headers) -> Result<Void, WSClientError> {
         guard let url = buildRequestUrl(path: urlPath, queryParams: [:]) else {
             return .failure(WSClientError.failedToBuildRequest(forUrlPath: urlPath))
         }
 
         let request = buildWSRequest(url: url, headers: headers())
-        let urlSession = URLSession(configuration: sessionConfig)
+        let urlSession = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
 
         self.keepConnected = .on(url: urlPath, headers: headers)
         webSocketTask = urlSession.webSocketTask(with: request)
@@ -57,12 +57,12 @@ public actor PublishedWSClient: IPublishedWSClient, IRequestBuilder {
          switch keepConnected {
          case let .on(url, headers):
              webSocketTask?.cancel()
-             _ = await connect(to: url, with: headers)
+             _ = connect(to: url, with: headers)
          case .off: break
          }
     }
 
-    public func disconnect() async {
+    public func disconnect() {
         keepConnected = .off
         webSocketTask?.cancel(with: .goingAway, reason: nil)
         webSocketTask = nil
@@ -78,13 +78,13 @@ public actor PublishedWSClient: IPublishedWSClient, IRequestBuilder {
         return await send(msg: msg)
     }
 
-    private func keepAlivePipeline() async {
+    private func keepAlivePipeline() {
         keepAliveSubscription = Timer.publish(every: Double(pingDelay), on: .main, in: .common).autoconnect()
             .sink { _ in
                 Task { [weak self] in
                     guard let self else { return }
-                    guard case .on = await self.keepConnected else { return }
-                    await self.sendPing()
+                    guard case .on = self.keepConnected else { return }
+                    self.sendPing()
                 }
             }
     }
@@ -93,9 +93,9 @@ public actor PublishedWSClient: IPublishedWSClient, IRequestBuilder {
         webSocketTask?.sendPing { [weak self] err in
             switch err {
             case .none:
-                print(">>>> ws ping")
+                print(">>> ws ping")
             case let .some(err):
-                print(">>>> ws ping err: \(err)")
+                print(">>> ws ping err: \(err)")
                 Task { [weak self] in
                     await self?.reconnect()
                 }
@@ -107,7 +107,7 @@ public actor PublishedWSClient: IPublishedWSClient, IRequestBuilder {
         Task {
             while let webSocketTask {
                 guard webSocketTask.closeCode == .invalid else {
-                    print(">>>> websocket: connection closed \(webSocketTask.closeCode)")
+                    print(">>> websocket: connection closed \(webSocketTask.closeCode)")
                     return
                 }
                 let result = await awaitNextMessage()
@@ -154,5 +154,15 @@ public actor PublishedWSClient: IPublishedWSClient, IRequestBuilder {
             print(">>> ws message send failed: \(msg) \(error)")
             return .failure(.failedToSend(msg: msg, cause: error))
         }
+    }
+}
+
+extension PublishedWSClient: URLSessionWebSocketDelegate {
+    public func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
+        print(">>> ws didOpenWithProtocol \(`protocol` ?? "")")
+    }
+
+    public func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
+        print(">>> ws didCloseWith \(closeCode) \(reason?.utf8 ?? "")")
     }
 }

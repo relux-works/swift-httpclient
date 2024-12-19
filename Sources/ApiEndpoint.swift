715dd18ea8cb7df4860e37148233a08f16226e33
Ivan Oparin <oparin@me.com>
2024-12-19 06:40:21 +0400

    Add ApiFullEndpoint type

--- Sources/ApiEndpoint.swift
+++ Sources/ApiEndpoint.swift
@@ -11,4 +11,31 @@ public struct ApiEndpoint: Sendable, Hashable {
         self.path = path
         self.type = type
     }
+    
+    init(from apiFullEndpoint: ApiFullEndpoint) {
+        self.path = apiFullEndpoint.url.absoluteString
+        self.type = apiFullEndpoint.type
+    }
+}
+
+#warning("Refactor library to use proper endpoint semantics")
+// in ApiEndpoint path should be path component not including base url
+public struct ApiFullEndpoint {
+    public let baseUrl: URL
+    public let path: String
+    public let type: ApiRequestType
+    
+    public init(
+        baseUrl: URL,
+        path: String,
+        type: ApiRequestType
+    ) {
+        self.baseUrl = baseUrl
+        self.path = path
+        self.type = type
+    }
+    
+    public var url: URL {
+        return baseUrl.appendingPathComponent(path)
+    }
 }

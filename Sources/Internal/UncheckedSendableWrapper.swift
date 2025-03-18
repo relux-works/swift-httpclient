3aaad1185751cfdf168bc8c4d7cf1e31c13369b4
Artem Grishchenko <alexis.ag85@gmail.com>
2025-03-18 21:26:06 +0300

    make unchecked sendable wrapper internal

---
+++ Sources/Internal/UncheckedSendableWrapper.swift
@@ -0,0 +1,15 @@
+internal struct UncheckedSendableWrapper<T>: @unchecked Sendable {
+    public let payload: T
+
+    public init(payload: T) {
+        self.payload = payload
+    }
+}
+
+extension Dictionary where Value == UncheckedSendableWrapper<Any> {
+    var payloads: [Key: Any] {
+        return reduce(into: [:]) { result, element in
+            result[element.key] = element.value.payload
+        }
+    }
+}

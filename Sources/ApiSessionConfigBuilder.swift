749477d078abd98f022e3dd1c08fa8ce2f6f1b97
Alexis Grigorev <alexis.ag85@gmail.com>
2025-06-16 21:34:56 +0300

    Update urlsession config for disabling cookies

--- Sources/ApiSessionConfigBuilder.swift
+++ Sources/ApiSessionConfigBuilder.swift
@@ -3,11 +3,15 @@ import Foundation
 public class ApiSessionConfigBuilder {
     public static func buildConfig(
             timeoutForResponse: Double,
-            timeoutResourceInterval: Double
+            timeoutResourceInterval: Double,
+            disableCookieStorage: Bool = false
     ) -> URLSessionConfiguration {
         let sessionConfig = URLSessionConfiguration.default
         sessionConfig.timeoutIntervalForRequest = timeoutForResponse
         sessionConfig.timeoutIntervalForResource = timeoutResourceInterval
+        if disableCookieStorage {
+            sessionConfig.httpCookieStorage = .none
+        }
         return sessionConfig
     }
 }
